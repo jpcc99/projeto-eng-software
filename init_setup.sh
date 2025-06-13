d41ea6f0635e12cee6ee42e3c2d772e40b6cbcc7
jpcc99 <jpcc.dev@gmail.com>
2025-06-13 14:52:42 -0300

    Automates the entire project setup

--- init_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Activate virtual enviroment
-# works for mac and linux. But we'll use docker so...
-source api_venv/bin/activate
-
-# install packages in virtual enviroment
-pip install -r requirements.txt

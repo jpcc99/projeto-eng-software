b4ab9a1e127152d0085772b6abb8bd3e7c4b7dae
jpcc99 <jpcc.dev@gmail.com>
2025-06-13 15:33:57 -0300

    Ch to production_config

--- template.py
+++ template.py
@@ -15,7 +15,7 @@ LIST_FILES = [
     "src/config/__init__.py",
     "src/config/config.py",
     "src/config/dev_config.py",
-    "src/config/production.py",
+    "src/config/production_config.py",
     # controllers
     "src/controllers/__init__.py",
     "src/controllers/auth_controller.py",

44af7e8f79f63aa1aee9322d2c19cd2d5949848a
jpcc99 <jpcc.dev@gmail.com>
2025-06-24 18:19:14 -0300

    Cria o responsável pelas rotas

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
+// Importa as rotas
+const routes = require('./routes/');
+
 const app = express();
 
+// Middlewares 
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true
@@ -15,6 +19,9 @@ app.use(express.json());
 
 // Conecta ao banco 
 require('./config/db');
+
+// Configura as rotas
+app.use('/api', routes);
 
 app.get('/', (_, res) => res.status(200).json({ message: "Hello World" }));
 

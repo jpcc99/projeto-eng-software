dee48f8935f57eb75378af12ea1fefc3a56e8012
jpcc99 <jpcc.dev@gmail.com>
2025-06-25 17:15:38 -0300

    Add a rota de cadastro e login

--- estoque_ai_backend/src/routes/index.js
+++ estoque_ai_backend/src/routes/index.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+// Importa as rotas
+const authRoutes = require('../routes/authRoutes');
+
+// Usa as rotas importadas
+router.use('/auth', authRoutes);
+
 module.exports = router;

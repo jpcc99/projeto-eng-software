79aa06a41d6ecabc9964e7a71352986efed9ca4f
jpcc99 <jpcc.dev@gmail.com>
2025-07-08 23:27:09 -0300

    Add materiais rotas e controllers

--- estoque_ai_backend/src/controllers/materialController.js
+++ estoque_ai_backend/src/controllers/materialController.js
@@ -1,5 +1,6 @@
 const { Material, UnidadeDeMedida } = require('../models/Material');
 const ApiResponse = require('../utils/apiResponse');
+const camposFaltando = require = require('../utils/camposFaltando');
 
 class MaterialController {
   static async criar(req, res) {
@@ -41,6 +42,55 @@ class MaterialController {
       return res.status(result.statusCode || 404).json(ApiResponse.error("Não foi possível listar materiais"));
     }
     return res.status(result.statusCode || 200).json(result);
+  }
+
+  static async buscar(req, res) {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(404).json(ApiResponse.error("Não foi possível encontrar material", 404));
+    }
+    try {
+      const result = await Material.buscarPorId(id);
+      if (!result.success) {
+        return res.status(404).json(ApiResponse.error("Não foi possível encontrar material", 404));
+      }
+      return res.status(200).json(result.data);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(404).json(ApiResponse.error("Não foi possível encontrar material", 404));
+    }
+  }
+
+  static async atualizar(req, res) {
+    const { id } = req.params;
+    let result = await camposFaltando(['id', 'nome', 'descricao', 'unidade_de_medida'], req.body);
+    if (!id || !result) {
+      return res.status(500).json(ApiResponse.error("Não foi possível atualizar material", 500));
+    }
+    const { nome, descricao, unidade_de_medida } = req.body;
+    try {
+      result = await Material.atualizar(id, nome, descricao, unidade_de_medida);
+      if (!result) {
+        throw Error(result.message);
+      }
+      return res.status(201).json(result.message);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).json(ApiResponse.error("Não foi possível atualizar material", 500));
+    }
+  }
+
+  static async remover(req, res) {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(500).json(ApiResponse.error("Não foi possível remover material", 500));
+    }
+    try {
+      const result = await Material.remover(id);
+      return res.status(201).json(result.message);
+    } catch (err) {
+      console.error
+    }
   }
 }
 
--- estoque_ai_backend/src/routes/materialRoutes.js
+++ estoque_ai_backend/src/routes/materialRoutes.js
@@ -4,9 +4,16 @@ const MaterialController = require('../controllers/materialController');
 const { authRequired, checaTipoDeUsuario } = require('../middlewares/authMiddleware');
 const EnumTiposUsuario = require('../utils/tipoUsuario');
 
-router.get('/', authRequired, MaterialController.listar);
-router.post('/cadastrar', authRequired, checaTipoDeUsuario(
+router.post('/', authRequired, checaTipoDeUsuario(
   EnumTiposUsuario.CONTROLE_MATERIAIS
 ), MaterialController.criar);
+router.get('/', authRequired, MaterialController.listar);
+router.get('/:id', authRequired, MaterialController.buscar);
+router.put('/:id', authRequired, checaTipoDeUsuario(
+  EnumTiposUsuario.CONTROLE_MATERIAIS
+), MaterialController.atualizar);
+router.delete('/:id', authRequired, checaTipoDeUsuario(
+  EnumTiposUsuario.CONTROLE_MATERIAIS
+), MaterialController.remover);
 
 module.exports = router;

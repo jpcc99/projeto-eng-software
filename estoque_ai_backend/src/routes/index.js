b9fdb04263e0c2b0ec9b80c833c1a01af9dadf9f
jpcc99 <jpcc.dev@gmail.com>
2025-07-09 00:28:25 -0300

    Passos finals para completar os endpoints

--- estoque_ai_backend/src/routes/index.js
+++ estoque_ai_backend/src/routes/index.js
@@ -4,11 +4,21 @@ const router = express.Router();
 // Importa as rotas
 const authRoutes = require('../routes/authRoutes');
 const setorRoutes = require('../routes/setorRoutes');
+const usuarioRoutes = require('../routes/usuarioRoutes');
 const materialRoutes = require('../routes/materialRoutes');
+const solicitacoesRoutes = require('../routes/solicitacoesRoutes');
+const planejamentoRoutes = require('../planejamentoRoutes');
+const relatorioRoutes = require('../relatorioRoutes');
+const notificacaoRoutes = require('../notificacaoRoutes');
 
 // Usa as rotas importadas
 router.use('/auth', authRoutes);
 router.use('/setor', setorRoutes);
+router.use('/usuario', usuarioRoutes);
 router.use('/material', materialRoutes);
+router.use('/solicitacao', solicitacoesRoutes);
+router.use('/planejamento', planejamentoRoutes);
+router.use('/relatorio', relatorioRoutes);
+router.use('/notificacoes', notificacaoRoutes);
 
 module.exports = router;

f67f9ff19b51a88b308ab7b150e3be02ebb2a846
jpcc99 <jpcc.dev@gmail.com>
2025-07-10 15:08:25 -0300

    Fix notficação controller call

--- estoque_ai_backend/src/routes/notificacaoRoutes.js
+++ estoque_ai_backend/src/routes/notificacaoRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const NotificacaoController = require('../controllers/notificacaoController');
+const NotificacaoController = require('../controllers/notficacaoController');
 const { authRequired } = require('../middlewares/authMiddleware');
 
 router.get('/', authRequired, NotificacaoController.listar);
